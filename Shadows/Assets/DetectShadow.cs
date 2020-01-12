5cec7b744acf1f8d4af6dc8adda1a445ed27f1bd
Lucas <lucasbittencourt@id.uff.br>
2020-01-11 23:53:33 -0400

    [ADD] Created the shadow detection script

---
+++ Shadows/Assets/DetectShadow.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Experimental.Rendering.Universal;
+
+
+public class DetectShadow : MonoBehaviour
+{
+
+    Light2D[] lights;
+
+    [SerializeField] LayerMask shadowCasters;
+    [SerializeField] Transform[] detectShadowPoints;
+
+    private bool IsOnShadow ;
+    
+    public bool isOnShadow
+    {
+        get { return IsOnShadow ; }
+        set
+        {
+            if( value == IsOnShadow )
+                return ;
+    
+            IsOnShadow = value ;
+            if( IsOnShadow )
+            {
+                Debug.Log("On Shadow");
+            } else{
+                Debug.Log("On Sun");
+            }    
+        }    
+    }
+
+
+    void Start()
+    {
+        lights = FindObjectsOfType<Light2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfInShadow();
+    }
+
+    void CheckIfInShadow()
+    {
+
+        for (int j = 0; j < detectShadowPoints.Length; j++)
+        {
+            
+            for (int i = 0; i < lights.Length ; i++)
+            {
+                if (!Physics2D.Raycast(detectShadowPoints[j].position, lights[i].transform.position - detectShadowPoints[j].position, shadowCasters))
+                {
+    
+                    // Debug.DrawLine(detectShadowPoints[j].position, lights[i].transform.position, Color.red);
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    isOnShadow = false;
+                    return;
+                }
+                else 
+                {
+                    // Debug.DrawLine(detectShadowPoints[j].position, lights[i].transform.position, Color.blue);
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                }   
+            }
+        }
+        isOnShadow = true;
+
+    }
+}
