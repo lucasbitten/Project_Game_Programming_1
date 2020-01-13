da7380d29bbf9f63599ab450424f228b5c03552f
Lucas <lucasbittencourt@id.uff.br>
2020-01-13 00:00:29 -0400

    [MOD] Polish detections script and added comments

--- Shadows/Assets/__Scripts/DetectShadow.cs
+++ Shadows/Assets/__Scripts/DetectShadow.cs
@@ -10,18 +10,14 @@ public class DetectShadow : MonoBehaviour
 
     Light2D[] lights;
 
-    [SerializeField] LayerMask shadowCasters;
-    [SerializeField] Transform[] detectShadowPoints;
-
-    private bool IsVisible ;
-
-    public Transform playerPosition;
-
-
-
-
+    
+    [SerializeField] LayerMask shadowCasters; // Layer masks with all the objects that cast shadows
+    [SerializeField] Transform[] detectShadowPoints; // Corners of the player to detect if he is at shadow or visible
     [SerializeField] Transform checkAngle;
 
+
+
+    private bool IsVisible;
     public bool isVisible
     {
         get { return IsVisible ; }
@@ -44,141 +40,105 @@ public class DetectShadow : MonoBehaviour
     void Start()
     {
         lights = FindObjectsOfType<Light2D>();
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         CheckIfInShadow();
 
-        // if(isVisible){
-
-        //     SendPlayerPositionIfOnSun();
-        // }
-
-        Debug.Log("Is visible = " + isVisible);
-
-    }
-
-    void SendPlayerPositionIfOnSun()
-    {
-        Debug.Log($"Player Position real time: {playerPosition.position}");
-    }
-
-    bool InLightSpot(Light2D light, Transform detectPoint){
-
-        for (int i = 0; i < lights.Length ; i++)
+        if(isVisible)
         {
 
-            Vector2 direction = transform.position - light.transform.position;
-            float startAngle = 90 - light.pointLightOuterAngle/2 +  light.transform.eulerAngles.z;
-            float endAngle = 90 + light.pointLightOuterAngle/2 +  light.transform.eulerAngles.z;
+            GetPlayerPosition();
+        }
+    }
 
-            if(startAngle >= 360){
-                startAngle -= 360;
-            }
+    void GetPlayerPosition()
+    {
+        Debug.Log($"Player Position real time: {transform.position}");
+    }
 
-            if(endAngle >= 360){
-                endAngle -= 360;
-            }
+    bool InLightSpot(Light2D light, Transform detectPoint) // Method to check if the player is at the light range
 
+    { 
+        // Angles where the cone of light begins and ends
+        float startAngle = 90 - light.pointLightOuterAngle/2 +  light.transform.eulerAngles.z;
+        float endAngle = 90 + light.pointLightOuterAngle/2 +  light.transform.eulerAngles.z;
 
-            float r = Mathf.Sqrt(Mathf.Pow(detectPoint.position.x - light.transform.position.x,2) + Mathf.Pow(detectPoint.position.y - light.transform.position.y,2));
-            float a = 180 + (Mathf.Atan2(light.transform.position.y - detectPoint.position.y, light.transform.position.x - detectPoint.position.x )) * Mathf.Rad2Deg;
-
-            // Debug.Log("R from Light " + lights[i].name + " = " + lights[i].pointLightOuterRadius);
-            // Debug.Log("r from Light " + lights[i].name + " = " + r);
-
-
-            if(r < light.pointLightOuterRadius){
-                //The player is inside the light radius
-                if (startAngle < endAngle){
-                    if (startAngle < a && a < endAngle){
-                        // Debug.Log("The corner " + detectPoint.name + " is at light " + i + " field of view");
-                        return true;
-                    } else {
-
-                        // Debug.Log("The corner " + detectPoint.name + " is not at light " + i + " field of view");
-                    }
-                } else{
-
-                    if (a > startAngle && a < endAngle){
-                        // Debug.Log("The corner " + detectPoint.name + " is at light " + i + " field of view");
-                        return true;
-                    } 
-                }
-
-                // Debug.Log("The corner " + detectPoint.name + " is not at light " + i + " field of view");
-            } 
-            // if (r > lights[i].pointLightOuterRadius){
-            //     //The player is outside of the light radius
-            //     Debug.Log("Outside slice, too far from the light!");
-            //     continue;
-            // } else if (startAngle < endAngle){
-            //     if ( startAngle < a && a < endAngle){
-            //         Debug.Log("Inside slice");
-            //     } else{
-
-            //         Debug.Log("Outside slice");
-            //         continue;
-            //     }
-
-            // } else if (startAngle > endAngle){
-            //     if (a > startAngle){
-            //         Debug.Log("Inside slice");
-            //     } else if ( a < endAngle){
-            //         Debug.Log("Inside slice");
-            //     } else{
-            //         Debug.Log("Outside slice");
-            //         continue;
-            //     }
-            // } else {
-            //     Debug.Log("Outside slice");
-            //     continue;
-            // }
-
-            // Debug.Log("start: " + startAngle);
-            // Debug.Log("end: " + endAngle);
-            // Debug.Log("Angle: " + (180 + Mathf.Atan2(lights[i].transform.position.y - checkAngle.position.y, lights[i].transform.position.x - checkAngle.position.x) * Mathf.Rad2Deg));
+        // Keep the angles always between 0 and 360
+        if(startAngle >= 360)
+        {
+            startAngle -= 360;
         }
 
+        if(endAngle >= 360)
+        {
+            endAngle -= 360;
+        }
+
+        // Converting the radius and the angle to polar coordinates
+        float r = Mathf.Sqrt(Mathf.Pow(detectPoint.position.x - light.transform.position.x,2) + Mathf.Pow(detectPoint.position.y - light.transform.position.y,2));
+
+        float angleBetweenPlayerAndLight = 180 + (Mathf.Atan2(light.transform.position.y - detectPoint.position.y,
+            light.transform.position.x - detectPoint.position.x )) * Mathf.Rad2Deg;
+
+        // Checking if the light can spot the player. If any corner is spottable  the whole player is spottable 
+
+        if(r < light.pointLightOuterRadius) // If The player is inside the light radius
+        { 
+
+            if (startAngle < endAngle)
+            {
+                if (startAngle < angleBetweenPlayerAndLight && angleBetweenPlayerAndLight < endAngle)
+                {
+
+                    return true; // One corner is spottable, so the player is also spottable 
+
+                }
+
+            } else if (angleBetweenPlayerAndLight > startAngle && angleBetweenPlayerAndLight < endAngle)
+            {
+
+                return true; // One corner is spottable, so the player is also spottable 
+            
+            }
+        }
+        // None of the corners is spottable so the player is not spottable
         return false;
 
     }
 
-    void CheckIfInShadow()
+    void CheckIfInShadow() // Method to check if the player is at any shadow
     {
-
+        // Check if all corners are at shadow
         for (int j = 0; j < detectShadowPoints.Length; j++)
         {
 
-            for (int i = 0; i < lights.Length ; i++){
+            // Check if the player is at shadow for all lights on scene
+            for (int i = 0; i < lights.Length ; i++)
+            {
 
-
-                if (InLightSpot(lights[i], detectShadowPoints[j]))
+                if (InLightSpot(lights[i], detectShadowPoints[j])) // Check for shadows only if the player is at the light range
                 {
-
+                    //If the player corner is behind something that cast shadows, continue the loop
                     if (Physics2D.Raycast(detectShadowPoints[j].position, lights[i].transform.position - detectShadowPoints[j].position, shadowCasters))
                     {
-                        
+
                         Debug.DrawLine(detectShadowPoints[j].position, lights[i].transform.position, Color.red);
                         gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
-                        // Debug.Log("The corner " + detectShadowPoints[j].name + " is behind something for the light " + lights[i].name);
-                    }
-                    else 
+
+                    } else // If the player corner is not behind something the whole player is visible, so isVisible = true and stop the method
                     {
                         Debug.DrawLine(detectShadowPoints[j].position, lights[i].transform.position, Color.blue);
                         gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
-                        // Debug.Log("The corner " + detectShadowPoints[j].name + " is visible for the light " + lights[i].name);
                         isVisible = true;
                         return;
                     }   
                 }
-
             }    
         }
-        
+
+        // If any of the corners are visible, the player is not visible, so isVisible = false
         isVisible = false;
 
     }
